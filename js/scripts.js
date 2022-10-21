d45b1d3f45fc2711dfeeaa61852818a5ac946707
Robert Onstott <roonstott@gmail.com>
2022-10-21 14:22:08 -0700

    Add Business Logic to save form input values as variables

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,53 @@ window.addEventListener("load", function() {
   const form = document.getElementById("survey");
   preReveal();
   form.addEventListener("submit", handleSubmit);
+  const reset = document.getElementById("resetButton");
+  reset.addEventListener("click", resetFn);
 });
 
-//Function Library
+    //UI Function Library
 
 function handleSubmit(event) {
   event.preventDefault();
-  document.getElementById("rustCard").style.backgroundColor = "green";
   postReveal();
+  setColors();
 }
 
 function preReveal() {
-  let hideFirst = document.getElementById("hiddenFirst");
-  let hideSecond = document.getElementById("hiddenSecond");
+  const hideFirst = document.getElementById("hiddenFirst");
+  const hideSecond = document.getElementById("hiddenSecond");
   hideFirst.setAttribute("class" , "hidden");
   hideSecond.setAttribute("class" , "");
 }
 
 function postReveal() {
-  let hideFirst = document.getElementById("hiddenFirst");
-  let hideSecond = document.getElementById("hiddenSecond");
+  const hideFirst = document.getElementById("hiddenFirst");
+  const hideSecond = document.getElementById("hiddenSecond");
   hideFirst.setAttribute("class" , "");
   hideSecond.setAttribute("class" , "hidden");
 }
 
+function resetFn() {
+  preReveal();
+  document.getElementById("rubyCard").style.backgroundColor = "aquamarine";
+  document.getElementById("c#Card").style.backgroundColor = "aquamarine";
+  document.getElementById("pythonCard").style.backgroundColor = "aquamarine";
+  document.getElementById("javascriptCard").style.backgroundColor = "aquamarine";
+  document.getElementById("rustCard").style.backgroundColor = "aquamarine";
+  document.getElementById("goCard").style.backgroundColor = "aquamarine";
+}
+
+function setColors(){
+  document.getElementById("rustCard").style.backgroundColor = "green"; //placeholder, complete after busienss logic and branching. 
+}
 
 //Business Logic
+
+function makeSuggestion() {
+  let q1Ans = document.getElementById("q1").value;
+  let q2Ans = document.getElementById("q2").value;
+  let q3Ans = document.getElementById("q3").value;
+  let q4Ans = document.querySelector("input[name='radA']:checked").value;
+  let q5Ans = document.querySelector("input[name='radB']:checked").value;
+  let q6Ans = document.querySelector("input[name='radC']:checked").value;
+}
