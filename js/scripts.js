98b7e3f5f36b59e0b14933fca4690b3a98b60f78
Robert Onstott <roonstott@gmail.com>
2022-10-21 13:28:31 -0700

    Set up JS load and submit handlers, and initial hide/reveal functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
 //User Interface Logic
 
-window.addEventListener("load" function() {
-  window.getElementById("form").addEventListener("submit", handleSubmit)
+window.addEventListener("load", function() {
+  const form = document.getElementById("survey");
+  preReveal();
+  form.addEventListener("submit", handleSubmit);
 });
 
+//Function Library
 
+function handleSubmit(event) {
+  event.preventDefault();
+  document.getElementById("rustCard").style.backgroundColor = "green";
+  postReveal();
+}
 
+function preReveal() {
+  let hideFirst = document.getElementById("hiddenFirst");
+  let hideSecond = document.getElementById("hiddenSecond");
+  hideFirst.setAttribute("class" , "hidden");
+  hideSecond.setAttribute("class" , "");
+}
 
+function postReveal() {
+  let hideFirst = document.getElementById("hiddenFirst");
+  let hideSecond = document.getElementById("hiddenSecond");
+  hideFirst.setAttribute("class" , "");
+  hideSecond.setAttribute("class" , "hidden");
+}
 
 
 //Business Logic
