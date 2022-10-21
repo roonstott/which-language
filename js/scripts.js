7844cc169098cbcf223ac7dc26cac6ac3c2f4abe
Robert Onstott <roonstott@gmail.com>
2022-10-21 15:16:43 -0700

    complete branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -47,10 +47,29 @@ function setColors(){
 //Business Logic
 
 function makeSuggestion() {
-  let q1Ans = document.getElementById("q1").value;
-  let q2Ans = document.getElementById("q2").value;
-  let q3Ans = document.getElementById("q3").value;
-  let q4Ans = document.querySelector("input[name='radA']:checked").value;
-  let q5Ans = document.querySelector("input[name='radB']:checked").value;
-  let q6Ans = document.querySelector("input[name='radC']:checked").value;
+  let q1Ans = parseInt(document.getElementById("q1").value);
+  let q2Ans = parseInt(document.getElementById("q2").value);
+  let q3Ans = parseInt(document.getElementById("q3").value);
+  let q4Ans = parseInt(document.querySelector("input[name='radA']:checked").value);
+  let q5Ans = parseInt(document.querySelector("input[name='radB']:checked").value);
+  let q6Ans = parseInt(document.querySelector("input[name='radC']:checked").value);
+  let topSum = q1Ans + q2Ans + q3Ans;
+  let bottomSum = q4Ans + q5Ans + q6Ans;
+  branchMachine(topSum, bottomSum);
+}
+
+function branchMachine(top, bottom) {
+  if (top <= 5 && bottom <= 5 ) {
+    return "Ruby"
+  } else if (top <= 5 && bottom > 5 && bottom < 10) {
+    return "C#"
+  } else if (top <= 5 && bottom >= 10) {
+    return "Python"
+  } else if (top > 5 && bottom <= 5) {
+    return "Javascript"
+  } else if (top > 5 && bottom > 5 && bottom < 10) {
+    return "Rust"
+  } else {
+    return "Go"
+  }
 }
